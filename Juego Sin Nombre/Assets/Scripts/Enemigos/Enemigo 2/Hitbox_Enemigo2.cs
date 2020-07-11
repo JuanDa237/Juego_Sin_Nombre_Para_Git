592d73149ce05b59dff5c4c47951e28df9821f15
JuanDa237 <juandavid200301@hotmail.com>
2020-07-11 15:24:14 -0500

    Version 0.0 Version donde ya hay mucha parte del juego adelantada.

---
+++ Juego Sin Nombre/Assets/Scripts/Enemigos/Enemigo 2/Hitbox_Enemigo2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox_Enemigo2 : MonoBehaviour
+{
+    public int dañoDeExplocion = 10;
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        if(collision.CompareTag("Hurtbox"))
+        {
+            collision.SendMessage("añadirDañoAlPadre", dañoDeExplocion);
+            gameObject.SendMessageUpwards("Explotar");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Juego_Sin_Nombre_Para_Git
 Este es mi segundo juego.
+Funciono!!!
